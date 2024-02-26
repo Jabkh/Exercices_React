9766ea8bbd3c956057262507a8dda4c5e46179df
JabKh <jaber.khaloua@hotmail.com>
2024-02-26 11:24:01 +0100

    9.1.2

--- src/components/Ecommerce/EcommerceApp.jsx
+++ src/components/Ecommerce/EcommerceApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Product from './Product';
 import Cart from './Cart';
 import { CartProvider } from './context/CartContext';
